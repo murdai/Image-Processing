dc085f9a3d09caeac9feaec4f7f34b36813ec336
murdai <dhakalsanjay09@gmail.com>
2020-01-08 13:04:28 +0545

    helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
-print("hello world")
+bikes = ['pulsar 200', 'honda 600', 'cbr 500','bmw','ducati','hyasung 2000']
+print("total bikes thta we have ",len(bikes),sep='---->')
+print(bikes)
+# #print(bikes[0])
+# bikes[2] = 'XL 300'
+# #print(bikes)
+# bikes.insert(1,"TNT xad")
+# #print(bikes)
+# del bikes[2]
+# #print (bikes)
+# del bikes[-1]
+# #print (bikes)
+# bikes.append('himalayas')
+# print(bikes)
